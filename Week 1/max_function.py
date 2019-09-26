c09e5835280115a490c55795d6fb9905c08e053d
Brian Lo <brianlo747@gmail.com>
2019-09-26 14:05:24 +0100

    added max_function

---
+++ Week 1/max_function.py
@@ -0,0 +1,11 @@
+# Define a to be 3.5
+a = 3.5
+
+# Define b to be 3.5 + a
+b = 3.5 + a
+
+# Reassign the maximum between a and b as the new a
+a = max(a,b)
+
+# Print what a is
+print("a is: {}".format(a))
